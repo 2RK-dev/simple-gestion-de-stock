2abec50508d0017990805eece889b741c037f4b7
rivon0507 <flavienrivon@gmail.com>
2026-03-23 20:04:59 +0300

    project init

---
+++ src/main/java/io/github/_2rkdev/gestiondestock/SimpleGestionDeStockApplication.java
@@ -0,0 +1,13 @@
+package io.github._2rkdev.gestiondestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGestionDeStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleGestionDeStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/_2rkdev/gestiondestock/SimpleGestionDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github._2rkdev.gestiondestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGestionDeStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
